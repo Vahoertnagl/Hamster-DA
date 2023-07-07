662d4dbe5ed4a446a1b105e62a11931038a03f29
xENNIxx <enni2004@gmx.at>
2023-07-07 20:04:04 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button
+      type="button"
+      class="hover:border-white border-2 border-transparent p-3 rounded-xl"
+      @click="count++"
+    >
+      count is {{ count }}
+    </button>
+  </div>
+</template>
